c56de2be86d4fe1c5d83e2ec788f11d90a2f4437
Hui Xiang <hui.xiang@daimler.com>
2020-10-27 09:14:13 +0100

    refactor: Add Comment 2

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 #comment 1: 1. Project
+#comment 2: Bikeshare
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
